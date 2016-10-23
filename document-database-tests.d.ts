6844cb4a7f71a677baa73c0d094664e8f2b5b367
Peter Snider <snider.peter@gmail.com>
2016-10-22 18:55:58 -0700

    added types: FieldsUsedInTests, UnsupportedUpdateCmds

--- document-database-tests.d.ts
+++ document-database-tests.d.ts
@@ -8,41 +8,45 @@ export interface Field {
     type: 'number' | 'string'
 }
 
-
+export interface FieldsUsedInTests {
+    // must refer to a top-level field that is not present and supports operator "+ 1" (either a string or a number)
+    populated_string?: string
+    // must refer to a top-level field that is a string, and is not present
+    unpopulated_string?: string
+    string_array?: {
+        name: string
+    }
+    obj_array?: {
+        name: string
+        key_field?: string
+        populated_field: Field
+        unpopulated_field?: Field
+        createElement: () => {}   // 2 sequential calls must return different results
+    }
+}
 
 export interface UnsupportedUpdateObjectCmds {
     set: boolean 
     unset: boolean
 }
+
 export interface UnsupportedUpdateArrayCmds {
     set: boolean 
     unset: boolean
     insert: boolean
     remove: boolean
 }
+
+export interface UnsupportedUpdateCmds {
+    object?: UnsupportedUpdateObjectCmds
+    array?: UnsupportedUpdateArrayCmds
+}
+
 export interface UpdateConfiguration {
     // controls which tests are skipped due to the object type not having the data type required by a test
-    test: {
-        // must refer to a top-level field that is not present and supports operator "+ 1" (either a string or a number)
-        populated_string?: string
-        // must refer to a top-level field that is a string, and is not present
-        unpopulated_string?: string
-        string_array?: {
-            name: string
-        }
-        obj_array?: {
-            name: string
-            key_field?: string
-            populated_field: Field
-            unpopulated_field?: Field
-            createElement: () => {}   // 2 sequential calls must return different results
-        }
-    },
+    test: FieldsUsedInTests,
     // controls which tests are skipped due to missing support
-    unsupported?: {
-        object?: UnsupportedUpdateObjectCmds
-        array?: UnsupportedUpdateArrayCmds
-    }
+    unsupported?: UnsupportedUpdateCmds
 }
 
 export function test_create<DocumentType extends {_id?: DocumentID}>(getDB: () => DocumentDatabase<DocumentType>, createNewObject: () => DocumentType, fieldnames: string[]): void
